25282b9371e220fd3c248266dd59aebdd39a5008
Connor Claxton <czclaxton@gmail.com>
2019-09-03 15:48:33 -0500

    added endpoints for getting comments from posts by id and getting posts by id

--- server.js
+++ server.js
@@ -1,26 +1,82 @@
 const express = require("express");
 
+actions = require("./data/db");
+
 const server = express();
 
 server.use(express.json());
 
-let posts = [
-  {
-    title: "How to train your dog",
-    contents: "Google it"
-  }
-];
+// server.get("/api/posts", (req, res) => {
+//   actions
+//     .find()
+//     .then(posts => {
+//       res.status(201).json(posts);
+//     })
+//     .catch(() => {
+//       res
+//         .status(500)
+//         .json({ error: "The posts information could not be retrieved." });
+//     });
+// });
 
-server.get("/api/posts", (req, res) => {
-  let result = [...posts];
+// server.get("/api/posts/:id", (req, res) => {
+//   const id = req.params.id;
+//   actions
+//     .findById(id)
+//     .then(post => {
+//       if (post.length != 0) {
+//         res.status(201).json(post);
+//       } else {
+//         res
+//           .status(404)
+//           .json({ error: "The post with the specified ID does not exist." });
+//       }
+//     })
+//     .catch(() => {
+//       res
+//         .status(500)
+//         .json({ error: "The post information could not be retrieved." });
+//     });
+// });
 
-  res.status(200).json(result);
-});
+// server.get("/api/posts/:id/comments", (req, res) => {
+//   const id = req.params.id;
+//   actions
+//     .findPostComments(id)
+//     .then(comments => {
+//       if (comments.length != 0) {
+//         res.status(201).json(comments);
+//       } else {
+//         res
+//           .status(404)
+//           .json({ error: "The comments information could not be retrieved." });
+//       }
+//     })
+//     .catch(() => {
+//       res
+//         .status(500)
+//         .json({ error: "The post information could not be retrieved." });
+//     });
+// });
 
-server.post("/api/posts", (req, res) => {
-  const post = req.body;
-  posts.push(post);
-  res.status(201).json(posts);
-});
+// server.post("/api/posts", (req, res) => {
+//   const post = req.body;
 
-module.exports = server;
+//   actions
+//     .insert(post)
+//     .then(() => {
+//       if (!post.title || !post.contents) {
+//         res.status(400).json({
+//           errorMessage: "Please provide title and contents for the post"
+//         });
+//       } else {
+//         res.status(201).json(post);
+//       }
+//     })
+//     .catch(() => {
+//       res.status(500).json({
+//         errorMessage:
+//           "There was an error while saving the post to the database."
+//       });
+//     });
+// });
